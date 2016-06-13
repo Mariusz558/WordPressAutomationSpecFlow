daa0c234f30bb2167ba8a02b20971d07432def8c
Mariusz558 <joga55@poczta.fm>
2016-06-13 23:04:31 +0200

    Feature file and Steps class added

---
+++ WordpressAutomationSpecFlow/Login.feature
@@ -0,0 +1,32 @@
+﻿Feature: Login
+	In order to write my diary
+	As a blogger
+	I want to be albe to log in
+
+@mytag
+Scenario: Positive Login
+	Given I am on the login page
+	And I have entered my credentials
+	When I press Log in button
+	Then I should be able to see my dashboard page
+
+@mytag
+Scenario: Negative Login - False User Name
+	Given I am on the login page
+	And I have entered unvalid user name
+	When I press Log in button
+	Then I should be able to see an error message "Invalid User Name"
+
+	@mytag
+Scenario: Negative Login - False Password
+	Given I am on the login page
+	And I have entered invalid password
+	When I press Log in button
+	Then I should be able to see an error message "Invalid Password"
+
+	@mytag
+Scenario: Negative Login - Invalid Credantials
+	Given I am on the login page
+	And I have entered invalid credentials
+	When I press Log in button
+	Then I should be able to see an error message "Invalid User Name"
---
+++ WordpressAutomationSpecFlow/LoginSteps.cs
@@ -0,0 +1,57 @@
+﻿using System;
+using TechTalk.SpecFlow;
+
+namespace WordpressAutomationSpecFlow
+{
+    [Binding]
+    public class LoginSteps
+    {
+        [Given(@"I am on the login page")]
+        public void GivenIAmOnTheLoginPage()
+        {
+            ScenarioContext.Current.Pending();
+        }
+        
+        [Given(@"I have entered my credentials")]
+        public void GivenIHaveEnteredMyCredentials()
+        {
+            ScenarioContext.Current.Pending();
+        }
+        
+        [Given(@"I have entered unvalid user name")]
+        public void GivenIHaveEnteredUnvalidUserName()
+        {
+            ScenarioContext.Current.Pending();
+        }
+        
+        [Given(@"I have entered invalid password")]
+        public void GivenIHaveEnteredInvalidPassword()
+        {
+            ScenarioContext.Current.Pending();
+        }
+        
+        [Given(@"I have entered invalid credentials")]
+        public void GivenIHaveEnteredInvalidCredentials()
+        {
+            ScenarioContext.Current.Pending();
+        }
+        
+        [When(@"I press Log in button")]
+        public void WhenIPressLogInButton()
+        {
+            ScenarioContext.Current.Pending();
+        }
+        
+        [Then(@"I should be able to see my dashboard page")]
+        public void ThenIShouldBeAbleToSeeMyDashboardPage()
+        {
+            ScenarioContext.Current.Pending();
+        }
+        
+        [Then(@"I should be able to see an error message ""(.*)""")]
+        public void ThenIShouldBeAbleToSeeAnErrorMessage(string p0)
+        {
+            ScenarioContext.Current.Pending();
+        }
+    }
+}
