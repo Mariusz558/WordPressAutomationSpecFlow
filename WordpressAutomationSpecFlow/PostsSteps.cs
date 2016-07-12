b93f056ee7396adeee18d96794eff88f7fb12bac
Mariusz558 <joga55@poczta.fm>
2016-07-12 22:37:30 +0200

    Add new post passes

--- WordpressAutomationSpecFlow/PostsSteps.cs
+++ WordpressAutomationSpecFlow/PostsSteps.cs
@@ -7,6 +7,7 @@ using NUnit.Framework;
 using OpenQA.Selenium.Support;
 using OpenQA.Selenium.Support.UI;
 using System.Threading;
+using System.Collections.Generic;
 
 namespace WordpressAutomationSpecFlow
 {
@@ -43,22 +44,21 @@ namespace WordpressAutomationSpecFlow
         public void WhenIEnterMyPost()
         {
             var firefox = ScenarioContext.Current["browser"] as FirefoxDriver;
-            //chrome.SwitchTo().Frame("wpbody-content");
-            //chrome.SwitchTo().Frame(chrome.FindElement(By.Id("wpbody-content")).SendKeys("Test Title"));
             firefox.FindElement(By.Id("title")).SendKeys("Test Title");
-            //firefox.SwitchTo().DefaultContent();
+            firefox.SwitchTo().Frame("content_ifr");
+            firefox.SwitchTo().ActiveElement().SendKeys("Test Content");
+            firefox.SwitchTo().DefaultContent();
             Thread.Sleep(3000);
-            //firefox.SwitchTo().Frame("content_ifr");
+            firefox.FindElement(By.Id("publish")).Click();
+
             //var contentFrame = firefox.FindElement(By.Id("content_ifr"));
             //contentFrame.SendKeys("Test Content");
-
             //firefox.SwitchTo().Frame(firefox.FindElement(By.Id("content_ifr")).SendKeys("Test Content"));
-            firefox.FindElement(By.XPath(".//*[@id='tinymce']/p")).SendKeys("Test Content");
+            //firefox.FindElement(By.XPath(".//*[@id='tinymce']/p")).SendKeys("Test Content");
             //Thread.Sleep(10);
             //chrome.Manage().Timeouts().ImplicitlyWait(TimeSpan.FromSeconds(10));
             //WebDriverWait publishButtonWait = new WebDriverWait(chrome, TimeSpan.FromSeconds(2));
             //ChromeWebElement publishButton = publishButtonWait.until(ExpectedConditions.ElementToBeClickable(By.Id("publish")));
-            firefox.FindElement(By.Id("publish")).Click();
             //chrome.Manage().Timeouts().ImplicitlyWait(TimeSpan.FromSeconds(10));
             //Thread.Sleep(10);
         }
@@ -66,9 +66,23 @@ namespace WordpressAutomationSpecFlow
         [Then(@"I should see it on the Posts page")]
         public void ThenIShouldSeeItOnThePostsPage()
         {
-            //var firefox = ScenarioContext.Current["browser"] as FirefoxDriver;
-            //firefox.FindElementByXPath(".//*[@id='menu-posts']/ul/li[2]/a").Click();
-            ////chrome.FindElements(By.TagName("tr"));
+            //bool postsListNotEmpty = Assert.IsNotEmpty(postsList);
+
+            var firefox = ScenarioContext.Current["browser"] as FirefoxDriver;
+            firefox.FindElementByXPath(".//*[@id='menu-posts']/ul/li[2]/a").Click();
+            IList<IWebElement> postsList = (firefox.FindElements(By.ClassName("row-title")));
+            Assert.IsNotEmpty(postsList, "Post does not exist. Fuck you!");
+            if(postsList.Count>0){//jesli jest kilka postow petla for each
+                //var postToTrash = firefox.FindElements(By.TagName("row-title"))[0];
+                IList<IWebElement> checkBoxes = firefox.FindElements(By.Name("post[]"));
+                var postToTrash = checkBoxes[0];
+                postToTrash.Click();
+                var actionsMenu = firefox.FindElement(By.Id("bulk-action-selector-top"));
+                var selectElement = new SelectElement(actionsMenu);
+                selectElement.SelectByText("Move to Trash");
+                var applyButton = firefox.FindElement(By.Id("doaction"));
+                applyButton.Click();
+            }
 
         }
 
